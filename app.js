af2a72cd2177a29a2f46f29e9667db98c5f284d7
Willemijn <willemijn@bresjer.nl>
2019-11-14 13:04:12 +0100

    Add my solution

--- app.js
+++ app.js
@@ -24,3 +24,75 @@ Includes everything in the School class, plus one additional property
 Properties: sportsTeams (array of strings)
 
 If you’re looking for a challenge, create the constructor() and getters for the four classes above. Then, use the setter and methods specifications in steps five, six, and seven to finish the project. */
+
+class School {
+    constructor(name, level, numberOfStudents) {
+      this._name = name;
+      this._level = level;
+      this._numberOfStudents = numberOfStudents;
+    }
+    
+    get name() {
+      return this._name;
+    }
+    
+    get level() {
+      return this._level;
+    }
+    
+    get numberOfStudents() {
+      return this._numberOfStudents;
+    }
+    
+    set numberOfStudents(val) {
+      if ( typeof val === 'number' ) {
+        this._numberOfStudents = val;
+      } else {
+        console.log('Invalid input: numberOfStudents must be set to a Number.')
+      }
+    }
+    
+    quickFacts() {
+      console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`)
+    }
+    
+    static pickSubstituteTeacher(substituteTeachers) {
+      let i = Math.floor(Math.random() * (substituteTeachers.length - 1));
+      return substituteTeachers[i];
+    }
+  }
+  
+  class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickUpPolicy) {
+      super(name, 'primary', numberOfStudents);
+      this._pickUpPolicy = pickUpPolicy;
+    }
+    
+    get pickUpPolicy() {
+      return this._pickUpPolicy;
+    }
+  }
+  
+  class MiddleSchool extends School {
+    constructor(name, level, numberOfStudents) {
+      super(name, level, numberOfStudents);
+    }
+  }
+  
+  class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+      super(name, 'high', numberOfStudents);
+      this._sportsTeams = sportsTeams;
+    }
+    
+    get sportsTeams() {
+      console.log(this._sportsTeams);
+    }
+  }
+  
+  const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+  lorraineHansbury.quickFacts();
+  console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']));
+  
+  const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+  alSmith.sportsTeams;
